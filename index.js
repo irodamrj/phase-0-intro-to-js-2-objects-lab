1fc1d9541f91b1b536d62f10ca0a11d69144c11e
Iroda Yılmaz <irodaand@hotmail.com>
2023-03-18 12:28:30 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+    name: "Sam",
+    streetAddress : "Eskişehir odunpazarı"
+};
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+ }
+
+ function updateEmployeeWithKeyAndValue(obj, key, value){
+    let newObj = {...obj};
+    newObj[key] = value;
+    return newObj;
+ }
+
+ function deleteFromEmployeeByKey(obj, key){
+    let newObj = {...obj};
+    delete newObj[key];
+    return newObj;
+ }
+
+ function destructivelyDeleteFromEmployeeByKey(obj,key){
+    delete obj[key];
+    return obj;
+ }
